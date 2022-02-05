2dd8ab3869ec1049eac6422d112a295fa8bf1bcf
Milena Michalska <milena.michalska@dac.digital>
2022-02-05 19:22:32 +0100

    added some plotting to lab 10

--- lab10/summary.py
+++ lab10/summary.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-path = '/home/milena/projects/studia/systemy-multimedialne/lab8/results.csv'
+path = '/home/milena/projects/studia/systemy-multimedialne/lab10/results.csv'
 data = pd.read_csv(path)
 
 rename_dict = {'Słowo identyfikujące' : 'id', 'Sygnatura czasowa': 'time'}
@@ -13,7 +13,6 @@ for column in data.columns:
         rename_dict[column] = column[13:-1]
 
 data = data.rename(columns=rename_dict).drop(columns=['time'])
-print(data.columns)
 
 colors = {
     'milena': 'yellow',
@@ -38,23 +37,40 @@ img3_cols = ['gauss3).', 'gauss10',
 
 img_cols = [img1_cols, img2_cols, img3_cols]
 
+def draw_plots(df):
+    for cols in img_cols:
+        cols.append('id')
+        data1 = df[cols]
+        for x, col in enumerate(data1.columns):
+            if col == 'id':
+                continue
+            for y, ind in enumerate(data1.index):
+                if data1.loc[ind, col]:
+                    plt.plot(col, data[col][ind], 'o', color=colors[data['id'][ind]])
+        
+        # d = np.polyfit(data1.columns, data1.index,1)
+        # f = np.poly1d(d)
+        # data1.insert(6,'Treg',f(data1.columns))
+        # data1.plot(x=data1.columns, y='Treg',color='red',ax=ax)
+
+        plt.xticks(range(len(data1.columns)), labels=data1.columns)
+        plt.show()
+
 #MOS dla obrazów - wszystkie
-for cols in img_cols:
-    cols.append('id')
-    data1 = data[cols]
-    for x, col in enumerate(data1.columns):
-        if col == 'id':
-            continue
-        for y, ind in enumerate(data1.index):
-            if data1.loc[ind, col]:
-                plt.plot(col, data[col][ind], 'o', color=colors[data['id'][ind]])
-            
-    plt.xticks(range(len(data1.columns)), labels=data1.columns)
-    plt.show()
+draw_plots(data)
 
 #MOS dla obrazów - zagregowane dla badanego
+data_agg_by_id = data.groupby('id').mean().reset_index()
+draw_plots(data_agg_by_id)
 
 #MOS dla obrazów - zagregowane
+data_agg = data.mean().reset_index()
+
+for i in range(3):
+    data1 = data_agg[i*9:((i+1)*9) - 1].reset_index(drop=True)
+    data1.plot(style='.')
+    plt.xticks(range(8), labels=data1['index'])
+    plt.show()
 
 def mse(org, modified):
     m, n, _ = org.shape
